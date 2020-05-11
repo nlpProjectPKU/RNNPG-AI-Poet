3d71561dc4f560bacac074d5a44b552628af0b43
kaname-madoka18 <43375270+kaname-madoka18@users.noreply.github.com>
2020-05-11 22:20:37 +0800

    Add files via upload

---
+++ classification.py
@@ -0,0 +1,40 @@
+import codecs
+from gensim.models import KeyedVectors
+
+wordvecPath = "D:\\pku\\2020春\\智能科学引论\\团队项目\\sgns.literature.word.bz2"
+sxhyPath = "D:\\pku\\2020春\\智能科学引论\\团队项目\\shixuehanying.txt"
+
+wordVec = KeyedVectors.load_word2vec_format(wordvecPath,\
+    binary = False, encoding = "utf-8", unicode_errors = "ignore")
+
+vocab = wordVec.wv.vocab
+shixuehanying = codecs.open(sxhyPath, encoding = 'UTF-8')
+
+classes = []
+for line in shixuehanying:
+    l = line.split()
+    if len(l) == 0 or not l[0][0].isdigit():
+        continue
+    for word in l[1:]:
+        if word in vocab:
+            classes.append(word)
+            break
+    else:
+        classes.append(None)
+
+def get_class(keywords):
+    sim = [1]*len(classes)
+    for word in keywords:
+        if not word in vocab:
+            continue
+        for i in range(len(classes)):
+            if classes[i] == None:
+                sim[i] = 0
+                continue
+            else:
+                sim[i] *= wordVec.wv.similarity(classes[i], word)
+    l = [(sim[i], i) for i in range(len(sim))]
+    l.sort(reverse = True)
+    return l[0][1], l[1][1]
+            
+
